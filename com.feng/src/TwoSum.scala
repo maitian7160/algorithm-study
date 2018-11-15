927d5ba4d38391b3a18565f0199c1afc28890ba5
zimu <hqf1320@qq.com>
2018-11-15 23:49:11 +0800

    two sum

--- com.feng/src/TwoSum.scala
+++ com.feng/src/TwoSum.scala
@@ -1,4 +1,3 @@
-
 /**
   * @Author: Feng
   * @Date: 2018/11/13
@@ -14,9 +13,14 @@ object TwoSum {
     * @param args
     */
   def main(args: Array[String]): Unit = {
-    val start = System.currentTimeMillis()
-    twoSum(Array(2, 7, 11, 15), 9)
-    println(System.currentTimeMillis() - start)
+
+    val t1 = new RunSeconds()
+    twoSum(Array(2, 7, 11, 15), 9).foreach(println(_))
+    println(t1.runTime)
+    val t2 = new RunSeconds()
+    twoTarget(Array(2, 7, 11, 15), 9).foreach(println(_))
+    println(t2.runTime)
+
   }
 
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
@@ -33,5 +37,34 @@ object TwoSum {
     arr
   }
 
+  /**
+    *
+    * @param arr
+    * @param target
+    */
+  final def twoTarget(arr: Array[Int], target: Int) = {
+    var i      = 0
+    var j      = arr.length - 1
+    var flag   = true
+    val result = Array(-1, -1)
+    while (flag && i < j) {
+      if (arr(i) + arr(j) > target) j = j - 1
+      else if (arr(i) + arr(j) < target) i = i + 1
+      else flag = false
+      result(0) = i
+      result(1) = j
+    }
+    result
+  }
+
+  /**
+    * run time  seconds
+    */
+  class RunSeconds {
+    val start        = System.currentTimeMillis()
+    lazy val end     = System.currentTimeMillis()
+    lazy val runTime = end - start
+
+  }
 
 }
